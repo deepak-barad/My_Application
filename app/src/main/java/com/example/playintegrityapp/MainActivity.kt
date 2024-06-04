4324aabfdfbf6d5d82eee6115d44a0adecab4181
deepak <deepak.and.dev@gmail.com>
2024-06-04 17:51:30 +0530

    Initial commit

--- app/src/main/java/com/example/playintegrityapp/MainActivity.kt
+++ app/src/main/java/com/example/playintegrityapp/MainActivity.kt
@@ -1,16 +1,23 @@
 package com.deepakbarad.playintegrityapp
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.play.core.integrity.IntegrityManagerFactory
 import com.google.android.play.core.integrity.StandardIntegrityManager.PrepareIntegrityTokenRequest
+import com.google.android.play.core.integrity.StandardIntegrityManager.StandardIntegrityToken
 import com.google.android.play.core.integrity.StandardIntegrityManager.StandardIntegrityTokenProvider
+import com.google.android.play.core.integrity.StandardIntegrityManager.StandardIntegrityTokenRequest
 
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var integrityTokenProvider: StandardIntegrityTokenProvider
+    lateinit var tvResult: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -21,13 +28,35 @@ class MainActivity : AppCompatActivity() {
             insets
         }
         setupPlayIntegrity()
+        tvResult = findViewById<TextView>(R.id.tvResult)
+
+        findViewById<TextView>(R.id.tvTapHere).setOnClickListener {
+            val requestHash = "2cp24z..."
+            val integrityTokenResponse =
+                integrityTokenProvider.request(
+                    StandardIntegrityTokenRequest.builder()
+                        .setRequestHash(requestHash)
+                        .build()
+                )
+            integrityTokenResponse
+                .addOnSuccessListener { response: StandardIntegrityToken ->
+                    sendToServer(
+                        response.token()
+                    )
+                }
+                .addOnFailureListener { exception: java.lang.Exception? -> handleError(exception) }
+        }
+    }
+
+    private fun sendToServer(token: String) {
+        tvResult.text = token
     }
 
     private fun setupPlayIntegrity() {
         val standardIntegrityManager =
             IntegrityManagerFactory.createStandard(applicationContext)
-        var integrityTokenProvider: StandardIntegrityTokenProvider
-        val cloudProjectNumber = 0L
+
+        val cloudProjectNumber = 652911097673
         standardIntegrityManager.prepareIntegrityToken(
             PrepareIntegrityTokenRequest.builder()
                 .setCloudProjectNumber(cloudProjectNumber)
@@ -41,5 +70,6 @@ class MainActivity : AppCompatActivity() {
 
     private fun handleError(exception: Exception?) {
         println(exception)
+        tvResult.text = "Exception $exception"
     }
 }
